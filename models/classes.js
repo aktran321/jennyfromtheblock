94989df31a863d90085bafe2e2a4682e0ea85189
Alex Simuro <aasimuro12@gmail.com>
2019-11-10 21:38:29 -0500

    deleted seeds, data for subject and class table

--- models/classes.js
+++ models/classes.js
@@ -3,6 +3,37 @@ module.exports = function(sequelize, DataTypes) {
     class_name: DataTypes.STRING
     });
 
+    Classes.create({ class_name: 'Human Biology' })
+    .then(function() {});
+    Classes.create({ class_name: 'Intro to Ecology and Evolution' })
+    .then(function() {});
+    Classes.create({ class_name: 'The Research Process' })
+    .then(function() {});
+    Classes.create({ class_name: 'Nutrition and Metabolism' })
+    .then(function() {});
+    Classes.create({ class_name: 'History of Biology' })
+    .then(function() {});
+    Classes.create({ class_name: 'Public Speaking' })
+    .then(function() {});
+    Classes.create({ class_name: 'Communication Research Methods' })
+    .then(function() {});
+    Classes.create({ class_name: 'Sports and The Digital' })
+    .then(function() {});
+    Classes.create({ class_name: 'Seminar in DIgital Rhetoric' })
+    .then(function() {});
+    Classes.create({ class_name: 'Small Group Communication' })
+    .then(function() {});
+    Classes.create({ class_name: 'Art of Literature' })
+    .then(function() {});
+    Classes.create({ class_name: 'Global Issues in Literature' })
+    .then(function() {});
+    Classes.create({ class_name: 'Intro to Creative Writing' })
+    .then(function() {});
+    Classes.create({ class_name: 'News Journalism' })
+    .then(function() {});
+    Classes.create({ class_name: 'Meth of Adv Literary Studies' })
+    .then(function() {});
+
     Classes.associate = function(models) {
     Classes.hasMany(models.AllData, {})
     Classes.belongsTo(models.Subjects, { 
--- models/subject.js
+++ models/subject.js
@@ -2,10 +2,19 @@ module.exports = function(sequelize, DataTypes) {
   var Subjects = sequelize.define("Subjects", {
     subject_name: DataTypes.STRING
   });
-    
+  Subjects.create({ subject_name: 'BIOL-Biology' })
+  .then(function() {});
+  Subjects.create({ subject_name: 'COMM-Communications' })
+  .then(function() {});
+  Subjects.create({ subject_name: 'ENGL-English' })
+  .then(function() {});
+
+
   Subjects.associate = function(models) {
     Subjects.hasMany(models.AllData, {});
     Subjects.hasMany(models.Classes, {});
   };
+
   return Subjects;
+  
 };
